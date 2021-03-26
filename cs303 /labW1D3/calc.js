9aedbd903b71689d6709f181672af4dca89075ee
cchala <79107461+cchala@users.noreply.github.com>
2021-03-25 20:40:21 -0500

    w1d4 Data type

--- cs303 /labW1D3/calc.js
+++ cs303 /labW1D3/calc.js
@@ -1,6 +1,7 @@
 "use strict"
-
-/**
+// /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+// module.exports = {calculator }; //add all of your function names here that you need for the node mocha tests
+ /**
  * @returns {Calculator} this is a constructor function
  */
 function Calculator() {
