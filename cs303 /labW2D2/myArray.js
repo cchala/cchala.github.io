c32b79a7cdfb78a99fe86f613b9039777027ba54
cchala <79107461+cchala@users.noreply.github.com>
2021-03-31 07:30:38 -0500

    update

--- cs303 /labW2D2/myArray.js
+++ cs303 /labW2D2/myArray.js
@@ -1,4 +1,4 @@
-"use strict"
+//"use strict"
  //module.exports = { oddValue, myMap, myreduce,myFilter,multiplyAll,sumValue, };
 /**
  * 
--- cs303 /labW2D2/myArrayTester.js
+++ cs303 /labW2D2/myArrayTester.js
@@ -1,4 +1,4 @@
-"use strict"
+//"use strict"
 // const assert = require("assert");  //always need this with node
 // const myExports = require("./myArray.js");
 // const myMap = myExports.myMap;
@@ -16,17 +16,20 @@ describe("higher oder practice", function () {
     // })
     it("multiply each element", function () {
         assert.deepEqual(myMap([1, 2, 3, 4, 5], multiplyAll), [1, 4, 9, 16, 25])
+        assert.deepEqual(myMap([0, 2, 5, 4, 1], multiplyAll), [0, 4, 25, 16, 1])
  
     })
  
     it("it filters odd elements", function () {
  
         assert.deepEqual(myFilter([1, 2, 3, 4, 5], oddValue), [1, 3, 5])
+        assert.deepEqual(myFilter([11, 1, 71, 4, 5], oddValue), [11,1, 71, 5])
     })
  
     it("it sums values", function () {
  
         assert.strictEqual(myreduce([1, 2, 3, 4, 5], sumValue), 15)
+        assert.strictEqual(myreduce([11, 9, 20, 10, 5], sumValue), 55)
     })
  
 })
