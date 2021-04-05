adee38f070f3bace39b07f2abb6e381d782eb39d
cchala <79107461+cchala@users.noreply.github.com>
2021-04-05 06:32:32 -0500

    update

--- cs303 /labW2D6/schedulCallTest.js
+++ cs303 /labW2D6/schedulCallTest.js
@@ -3,21 +3,21 @@
 // const myExports = require("./schedulCall.js");
 
 
-let bankObj;
-it("Bank Transaction", function(){
-    function makeBank() {
-        const bank = {
-        transactionsDB: [],
-        };
-        return function(){
-        return bank
-        };
-       }
-       let bankObj = makeBank()();
-         bankObj.transactionsDB = [
-        { customerId: 1, customerTransactions: [10, 50, -40] },
-        { customerId: 2, customerTransactions: [10, 10, -10] },
-        { customerId: 3, customerTransactions: [5, -5, 55] }];
+// let bankObj;
+// it("Bank Transaction", function(){
+//     function makeBank() {
+//         const bank = {
+//         transactionsDB: [],
+//         };
+//         return function(){
+//         return bank
+//         };
+//        }
+//        let bankObj = makeBank()();
+//          bankObj.transactionsDB = [
+//         { customerId: 1, customerTransactions: [10, 50, -40] },
+//         { customerId: 2, customerTransactions: [10, 10, -10] },
+//         { customerId: 3, customerTransactions: [5, -5, 55] }];
     
-    assert.deepEqual(bankObj.custId(3), {customerId: 3, customerTransactions: [5, -5, 55] });
-  });
+//     assert.deepEqual(bankObj.custId(3), {customerId: 3, customerTransactions: [5, -5, 55] });
+//   });
