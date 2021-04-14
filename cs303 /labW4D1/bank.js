1a0491baf818dfb2f4439d12efa32e5f1b625ef4
cchala <79107461+cchala@users.noreply.github.com>
2021-04-13 21:09:42 -0500

    update

--- cs303 /labW4D1/bank.js
+++ cs303 /labW4D1/bank.js
@@ -1,12 +1,67 @@
-  
-"use strict";
-/* global exports require Account   SavingsAccount CheckingAccount */
+"use strict"
 
-/* exports at end of file since exporting an object, which can only be referenced after definition   */
-    
-const acc = require("./account.js");//with node need the name of your file with your functions here
-const Account = acc.Account;  //do this for all of the functions used in the Mocha tests
-const chk = require("./checkingaccount.js");
-const CheckingAccount = chk.CheckingAccount;
-const sav = require("./savingsaccount.js");
-const SavingsAccount = sav.SavingsAccount; 
+// const { Account } = require("./account");
+// const { SavingsAccount } = require("./savingsaccount");
+// const { CheckingAccount } = require("./checkingaccount");
+class Bank{
+    static nextNumber=0;
+    /**
+     * Bank constructor
+     */
+    constructor(){
+        Bank.nextNumber++;
+        this. _account=[];
+    }
+    /**
+     * add accounts
+     */
+    addAccount(){
+       this._account.push(new Account(Bank.nextNumber++))
+    }
+    /**
+     * 
+     * @param {Number} interest is interest rate
+     */
+    addSavingsAccount(interest){
+        this._account.push(new SavingsAccount(Bank.nextNumber++,interest))
+    }
+    /**
+     * 
+     * @param {Number} overdraft is over draft
+     */
+    addCheckingAccount(overdraft){
+        this._account.push(new CheckingAccount(Bank.nextNumber++,overdraft))
+    }
+   /**
+    * 
+    * @param {Number} number  is account number
+    */
+    closeAccount(number){
+        let index=this._account.findIndex((item)=>item._number===number)
+        this._account.splice(index,1)
+    }
+    /**
+     * 
+     * @returns bank reports 
+     */
+    accountReport(){
+      let accountReport=""  
+      for(let account of this._account ){
+        accountReport+=account.toString()+"\n"
+      }
+      return accountReport;
+    }
+}
+
+exports.Bank = Bank;
+ //console.log(Bank.nextNumber)//0
+//  let bank=new Bank()
+//  bank.addAccount()
+//  bank.addCheckingAccount(500)
+// bank.addSavingsAccount(5)
+// console.log( bank.accountReport())
+// console.log(new SavingsAccount().toString())
+// console.log(bank._account)
+// console.log("+++++++++++++++++++++++++++=\n")
+// bank.closeAccount(2)
+// console.log(bank._account)
--- cs303 /labW4D1/bankTests.js
+++ cs303 /labW4D1/bankTests.js
@@ -1,27 +1,25 @@
 "use strict";
-/* global require describe it assert  Account SavingsAccount CheckingAccount Bank*/
-
-/* You need the assert and function name declarations to test in node.  
-Comment these out when you send it to the browser with the index.html mocha setup page.   */
-
-const assert = require("assert");  //always need this with node
-
-const acc = require("./account.js");//with node need the name of your file with your functions here
-const Account = acc.Account;  //do this for all of the functions used in the Mocha tests
-const bnk = require("./bank.js");
-const Bank = bnk.Bank;
-const chk = require("./checkingaccount.js");
-const CheckingAccount = chk.CheckingAccount;
-const sav = require("./savingsaccount.js");
-const SavingsAccount = sav.SavingsAccount;  
+/* global describe */
+/* global it */
+/* global assert */
+/* global Account */
+/* global SavingsAccount */
+/* global CheckingAccount */
+/* global Bank */
+/* global require */
+// const assert = require("assert");
+// const acc = require("./account.js");
+// const bnk = require("./bank.js");
+// const chk = require("./checkingaccount.js");
+// const sav = require("./savingsaccount.js");
 
 describe("Account class", () => {
     let account = undefined;
     describe("constructor(number)", () => {
         it("takes a number which becomes the account number", () => {
-            account = new Account(1);
+            account = new acc.Account(1);
             assert.equal(account.getNumber(), 1);
-            assert.equal(account.constructor, Account);
+            assert.equal(account.constructor, acc.Account);
         });
     });
 
@@ -91,10 +89,10 @@ describe("SavingsAccount", () => {
     let savings = undefined;
     describe("constructor(number, interest)", () => {
         it("takes a number and an interest rate and makes a SavingsAccount", () => {
-            savings = new SavingsAccount(1, 5);
+            savings = new sav.SavingsAccount(1, 5);
             assert.equal(savings.getNumber(), 1);
             assert.equal(savings.getInterest(), 5);
-            assert.equal(savings.constructor, SavingsAccount);
+            assert.equal(savings.constructor, sav.SavingsAccount);
         });
     });
     describe("interest getter / setter", () => {
@@ -125,7 +123,7 @@ describe("SavingsAccount", () => {
     describe("endOfMonth() method", ()=>{
         it("returns a string saying that interest was added", ()=>{
             savings.withdraw(5);
-            assert.equal(savings.endOfMonth(), "Interest added SavingsAccount 1: balance: 105 interest: 5");
+            assert.equal(savings.endOfMonth(), "Interest added SavingsAccount 1: balance: 100 interest: 5");
         });
     });
 
@@ -135,10 +133,10 @@ describe("CheckingAccount", () => {
     let checking = undefined;
     describe("constructor(number, overdraft)", () => {
         it("takes a number and the overdraft limit and makes a checking account", () => {
-            checking = new CheckingAccount(1, 200);
+            checking = new chk.CheckingAccount(1, 200);
             assert.equal(checking.getNumber(), 1);
             assert.equal(checking.getOverdraft(), 200);
-            assert.equal(checking.constructor, CheckingAccount);
+            assert.equal(checking.constructor, chk.CheckingAccount);
         });
     });
     describe("overdraft getter / setter", () => {
@@ -170,12 +168,12 @@ describe("CheckingAccount", () => {
     });
     describe("toString() method", () => {
         it("returns a string representation of the SavingsAccount", () => {
-            assert.equal("CheckingAccount 1: balance: -200 overdraft limit: 250", checking.toString());
+            assert.equal("CheckingAccount 1: balance: -300 overdraft limit: 250", checking.toString());
         });
     });
     describe("endOfMonth() method", ()=>{
         it("returns a warning string when the balance is below zero", ()=>{
-            assert.equal(checking.endOfMonth(), "Warning, low balance CheckingAccount 1: balance: -200 overdraft limit: 250");
+            assert.equal(checking.endOfMonth(), "Warning, low balance CheckingAccount 1:balance: -300 overdraft limit: 250");
         });
         it("returns an empty string when balance above zero", ()=>{
             checking.deposit(1000);
@@ -190,8 +188,8 @@ describe("Bank Class", () => {
     let bank = undefined;
     describe("constructor()", () => {
         it("creates a Bank object", () => {
-            bank = new Bank();
-            assert.equal(bank.constructor, Bank);
+            bank = new bnk.Bank();
+            assert.equal(bank.constructor, bnk.Bank);
             assert.equal(bank._accounts.length, 0);
         });
     });
